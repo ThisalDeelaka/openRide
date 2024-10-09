c86f601c4998b83c45b8acc537984ecd1c88a7a0
Malisha Sandeep <it22355232@my.sliit.lk>
2024-10-09 18:34:04 +0530

    custom button worked

--- frontend/app/index.jsx
+++ frontend/app/index.jsx
@@ -7,19 +7,19 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 export default function App() {
   return (
     <SafeAreaView>
-    <View className="p-6">
-      <Text className="text-3xl font-bold mb-6">open ride</Text>
-      <StatusBar style="auto" />
-      
-      <CustomButton
-        title="Sign In"
-        handlePress={() => router.push('/sign-in')}
-      />
-      <CustomButton
-        title="Sign Up"
-        handlePress={() => router.push('/sign-up')}
-      />
-    </View>
+      <View className="p-6">
+        <Text className="text-3xl font-bold mb-6">open ride</Text>
+        <StatusBar style="auto" />
+        
+        <CustomButton
+          title="Sign In"
+          onPress={() => router.push('/sign-in')} // Changed handlePress to onPress
+        />
+        <CustomButton
+          title="Sign Up"
+          onPress={() => router.push('/sign-up')} // Changed handlePress to onPress
+        />
+      </View>
     </SafeAreaView>
   );
 }
